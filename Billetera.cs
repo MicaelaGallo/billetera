e26f849a61703514b8d4ebae1fd27ed4b8356a9a
Micaela Gallo <micanorali@gmail.com>
2022-10-26 21:19:38 -0300

    Agregar archivos de proyecto.

---
+++ Billetera.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercbilletera
+{
+    internal class Billetera
+    { 
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal Total()
+        {
+            decimal acumulador = 0;
+
+            acumulador += BilletesDe10 * 10;
+            acumulador += BilletesDe20 * 20;
+            acumulador += BilletesDe50 * 50;
+            acumulador += BilletesDe100 * 100;
+            acumulador += BilletesDe200 * 200;
+            acumulador += BilletesDe500 * 500;
+            acumulador += BilletesDe1000 * 1000;
+            return acumulador;
+        }
+
+       public Billetera Combinar (Billetera SegundaBilletera)
+        {
+            var billeteraNueva= new Billetera();
+            int SumaBill10 = BilletesDe10 + SegundaBilletera.BilletesDe10;
+            int SumaBill20 = BilletesDe20 + SegundaBilletera.BilletesDe20;
+            int SumaBill50 = BilletesDe50 + SegundaBilletera.BilletesDe50;
+            int SumaBill100 = BilletesDe100 + SegundaBilletera.BilletesDe100;
+            int SumaBill200 = BilletesDe200 + SegundaBilletera.BilletesDe200;
+            int SumaBill500 = BilletesDe500 + SegundaBilletera.BilletesDe500;
+            int SumaBill1000 = BilletesDe1000 + SegundaBilletera.BilletesDe1000;
+
+            billeteraNueva.BilletesDe10 = SumaBill10;
+            billeteraNueva.BilletesDe20 = SumaBill20;
+            billeteraNueva.BilletesDe50 = SumaBill50;
+            billeteraNueva.BilletesDe100 = SumaBill100;
+            billeteraNueva.BilletesDe200 = SumaBill200;
+            billeteraNueva.BilletesDe500=SumaBill500;
+            billeteraNueva.BilletesDe1000=SumaBill1000;
+
+            if(billeteraNueva!= null)
+            {
+                BilletesDe10 = 0;
+                BilletesDe20 = 0;
+                BilletesDe50 = 0;
+                BilletesDe100 = 0;
+                BilletesDe200 = 0;
+                BilletesDe500 = 0;
+                BilletesDe1000 = 0;
+                SegundaBilletera.BilletesDe10 = 0;
+                SegundaBilletera.BilletesDe20 = 0;
+                SegundaBilletera.BilletesDe50 = 0;
+                SegundaBilletera.BilletesDe100 = 0;
+                SegundaBilletera.BilletesDe200 = 0;
+                SegundaBilletera.BilletesDe500 = 0;
+                SegundaBilletera.BilletesDe1000 = 0;
+            }
+               
+            return billeteraNueva; 
+
+
+        }
+    }
+
+
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+using ejercbilletera; 
+
+var Billetera1 = new Billetera();
+Console.WriteLine("Billetera 1 ");
+Console.WriteLine("Ingrese la cantidad de billetes de 10");
+int billete10Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe10 = billete10Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 20");
+int billete20Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe20 = billete20Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 50");
+int billete50Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe50 = billete50Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 100");
+int billete100Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe100 = billete100Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 200");
+int billete200Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe200 = billete200Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 500");
+int billete500Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe500 = billete500Ingresado;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 1000");
+int billete1000Ingresado = int.Parse(Console.ReadLine());
+    Billetera1.BilletesDe1000 = billete1000Ingresado;
+Console.Clear();
+Console.WriteLine($"El total de plata de billetera 1 es { Billetera1.Total()}");
+Console.WriteLine();
+
+
+var Billetera2 = new Billetera();
+Console.WriteLine("Billetera 2 ");
+Console.WriteLine("Ingrese la cantidad de billetes de 10");
+int billete10Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe10 = billete10Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 20");
+int billete20Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe20 = billete20Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 50");
+int billete50Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe50 = billete50Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 100");
+int billete100Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe100 = billete100Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 200");
+int billete200Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe200 = billete200Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 500");
+int billete500Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe500 = billete500Ingresado2;
+
+Console.WriteLine("Ingrese la cantidad de billetes de 1000");
+int billete1000Ingresado2 = int.Parse(Console.ReadLine());
+    Billetera2.BilletesDe1000 = billete1000Ingresado2;
+
+Console.WriteLine(Billetera2.Total());
+
+Console.Clear();
+Console.WriteLine($"El total de plata de billetera 1 es {Billetera1.Total()}");
+Console.WriteLine();
+Console.WriteLine($"El total de plata de billetera 2 es {Billetera2.Total()}");
+Console.WriteLine();
+
+
+
+Billetera Billetera3 = Billetera1.Combinar(Billetera2);
+
+Console.WriteLine($"Combinamos las dos billeteras, el total de la billetera 3 es { Billetera3.Total()}");
+
+Console.WriteLine($"El total de la billetera 1 es {Billetera1.Total()}" );
+Console.WriteLine($"El total de la billetera 2 es {Billetera2.Total()}") ;
